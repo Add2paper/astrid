59ad986580b34ff040f05af272da0f13b7553322
Sin-Woo Bang <sinwoobang@gmail.com>
2015-09-01 11:15:18 +0900

    Changed json dumps' indent None to 2
    Hi, I changed json dumps' indent None to 2.
    It looks well and make development easier when to use in future.

--- astrid/http.py
+++ astrid/http.py
@@ -24,7 +24,7 @@ def response(body, content_type='text/html; charset=utf-8'):
         kwargs['body'] = body.encode('utf-8')
     elif isinstance(body, dict):
         kwargs['content_type'] = 'application/json; charset=utf-8'
-        kwargs['body'] = json.dumps(body).encode('utf-8')
+        kwargs['body'] = json.dumps(body, indent=2).encode('utf-8')
     elif isinstance(body, int) or isinstance(body, float):
         kwargs['body'] = str(body).encode('utf-8')
 
